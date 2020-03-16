4c079b732bc162e96dcb2acaaadad4a9f1514910
Caitlin Sigler <csigler@scu.edu>
2020-03-16 11:40:09 -0700

    added food and tips

---
+++ galapagosScript.js
@@ -0,0 +1,9 @@
+function redirectHome() {
+  window.location.href = "index.html";
+}
+function redirectFood() {
+  window.location.href = "food.html";
+}
+function redirectTips() {
+  window.location.href = "tips.html";
+}
